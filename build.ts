0e591918789385a5b43736684c4d456f26971326
MarioMH8 <mhidalgomario@gmail.com>
2023-10-18 20:12:15 +0200

    ci: fix react support

--- build.ts
+++ build.ts
@@ -12,6 +12,16 @@ await Promise.all([
 		entryNames: 'google-analytics',
 	}),
 	esbuild.build({
+		entryPoints: ['src/react.ts'],
+		outdir: './dist',
+		format: 'esm',
+		platform: 'node',
+		target: 'esnext',
+		bundle: true,
+		entryNames: 'google-analytics-react',
+		external: ['react', 'react-dom'],
+	}),
+	esbuild.build({
 		entryPoints: ['src/index.ts'],
 		outdir: './dist',
 		// @ts-expect-error umd plugin is not typed
@@ -22,5 +32,18 @@ await Promise.all([
 		minify: true,
 		entryNames: 'google-analytics.umd.min',
 		plugins: [umd()],
+	}),
+	esbuild.build({
+		entryPoints: ['src/react.ts'],
+		outdir: './dist',
+		// @ts-expect-error umd plugin is not typed
+		format: 'umd',
+		platform: 'browser',
+		target: 'esnext',
+		bundle: true,
+		minify: true,
+		entryNames: 'google-analytics-react.umd.min',
+		plugins: [umd()],
+		external: ['react', 'react-dom'],
 	}),
 ]);
